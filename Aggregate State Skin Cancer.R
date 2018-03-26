ed39dfa05e09f6238fe361faa1e757e19f182ffe
samdeet <samdeet.khan98@gmail.com>
2018-03-26 14:20:35 -0700

    Delete Aggregate State Skin Cancer.R

--- Aggregate State Skin Cancer.R
+++
@@ -1,8 +0,0 @@
-data <- read.csv(file = "skincancer.csv", header = TRUE);
-total <- aggregate(population ~ state, data = data, sum);
-states <- aggregate(count ~ state, data = data, sum);
-states$count <- states$count * 100000;
-states$population <- total$population;
-new_data <- transform(states, rate=count/population)
-regression <- lm(rate ~ state, data = new_data)
-summary(regression)
