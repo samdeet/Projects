5ba808ea26b9a1e723848719b9efffbf7ba3513a
samdeet <samdeet.khan98@gmail.com>
2018-03-26 14:26:05 -0700

    Add files via upload

---
+++ Mobile Medic/Unity Scripts/MoveGraph.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveGraph : MonoBehaviour {//rename
+
+
+    public GameObject graphOBJ;
+    private GameObject obj;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+        transform.position = new Vector3(0, 0, 0);
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            graphOBJ = Resources.Load("graph") as GameObject;
+            obj = Instantiate(graphOBJ, Vector3.zero, Quaternion.identity) as GameObject;
+            obj.AddComponent<Movement>();
+
+        }
+
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            Destroy(obj);
+        }
+
+	}
+}
---
+++ Mobile Medic/Unity Scripts/Movement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public float rotSpeed = 50f;
+    public float scaleDown = 0.5f;
+    public float scaleUp = 2f;
+
+    // Use this for initialization
+    void Start()
+    {
+        transform.localScale = new Vector3(scaleDown, scaleDown, scaleDown);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            transform.Rotate(Vector3.right * Time.deltaTime * rotSpeed * Input.GetAxis("Horizontal"));
+        }
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            transform.Rotate(Vector3.up * Time.deltaTime * rotSpeed * Input.GetAxis("Vertical"));
+        }
+         if (Input.GetAxis("Vertical") != 0)
+        {
+            transform.Rotate(Vector3.up * Time.deltaTime * rotSpeed * Input.GetAxis("Vertical"));
+        }
+        if (Input.GetAxis("DPad-H") > 0)
+        {
+            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z) * 1.01f;
+        }
+        if (Input.GetAxis("DPad-H") < 0)
+        {
+            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z) * 0.99f;
+        }
+        if (Input.GetAxis("DPad-V") > 0)
+        {
+            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y * 1.01f, transform.localScale.z);
+        }
+        if (Input.GetAxis("DPad-V") < 0)
+        {
+            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y * 0.99f, transform.localScale.z);
+        }
+    }
+
+}
