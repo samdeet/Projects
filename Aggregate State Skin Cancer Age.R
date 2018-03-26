a5deed858aa3ec70ba9d00be3595e681598ed0a2
samdeet <samdeet.khan98@gmail.com>
2018-03-26 14:20:16 -0700

    Delete Aggregate State Skin Cancer Age.R

--- Aggregate State Skin Cancer Age.R
+++
@@ -1,9 +0,0 @@
-data1 = read.csv(file = "skincancer.csv", header = TRUE);
-total1 = aggregate(population ~ agegroupcode, data = data, sum);
-agegroup1 = aggregate(count ~ agegroupcode, data = data, sum);
-agegroup1$count <- agegroup1$count * 100000;
-agegroup1$population <- total1$population;
-new_data1 <- transform(agegroup1, rate=count/population)
-regression <- lm(rate ~ agegroupcode, data = new_data1)
-
-#model diagnostics
